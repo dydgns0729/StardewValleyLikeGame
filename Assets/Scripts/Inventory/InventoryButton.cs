b6f2674883f642e74035657a9e1f5fab091d493a
toneed <dydgns0729@gmail.com>
2025-02-28 13:24:01 +0900

    1. 드래그앤 드롭 구현 ( 클릭과 같이 사용가능) 2. 코드 정리

--- Assets/Scripts/Inventory/InventoryButton.cs
+++ Assets/Scripts/Inventory/InventoryButton.cs
@@ -2,93 +2,87 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-
 namespace MyStardewValleylikeGame
 {
-    // 인벤토리 슬롯(버튼)을 관리하는 클래스
-    public class InventoryButton : MonoBehaviour, IPointerClickHandler, /*IBeginDragHandler, IDragHandler, IEndDragHandler,*/ IDropHandler
+    public class InventoryButton : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
     {
-        [SerializeField]
-        Image icon;  // 아이템 아이콘 이미지
+        #region Variables
+        [SerializeField] private Image icon;            // 아이템 아이콘을 나타내는 Image 컴포넌트
+        [SerializeField] private TextMeshProUGUI text;  // 아이템 개수를 표시하는 TextMeshProUGUI 컴포넌트
+        public int myIndex;                             // 현재 버튼의 인덱스
 
-        [SerializeField]
-        TextMeshProUGUI text;  // 아이템 개수를 표시하는 텍스트
+        private GameManager gameManager;                //게임매니저 참조변수
+        #endregion
 
-        //int myIndex;  // 해당 버튼의 인벤토리 슬롯 인덱스
-        public int myIndex;  // 해당 버튼의 인벤토리 슬롯 인덱스
+        //게임 매니저를 필드로 참조
+        private void Start()
+        {
+            //게임매니저 할당
+            gameManager = GameManager.Instance;
+        }
 
-        // 슬롯의 인덱스를 설정하는 함수
+        // 인덱스를 설정하는 메서드
         public void SetIndex(int index) => myIndex = index;
 
-        // 슬롯에 아이템을 설정하는 함수
+        // 슬롯의 아이템 정보를 버튼에 설정하는 메서드
         public void SetItem(ItemSlot slot)
         {
-            icon.gameObject.SetActive(true);  // 아이콘 활성화
-            icon.sprite = slot.item.icon;  // 아이콘 이미지 설정
-
-            if (slot.item.stackable)  // 아이템이 스택 가능한 경우
-            {
-                text.gameObject.SetActive(true);  // 개수 텍스트 활성화
-                text.text = slot.count.ToString();  // 개수 표시
-            }
-            else  // 스택 불가능한 아이템 (도구 등)
-            {
-                text.gameObject.SetActive(false);  // 개수 텍스트 비활성화
-                text.text = "";  // 텍스트 초기화
-            }
+            icon.gameObject.SetActive(true);                                // 아이콘을 활성화
+            icon.sprite = slot.item.icon;                                   // 슬롯의 아이템 아이콘 설정
+            text.gameObject.SetActive(slot.item.stackable);                 // 아이템이 스택 가능한 경우 텍스트 활성화
+            text.text = slot.item.stackable ? slot.count.ToString() : "";   // 스택 가능한 경우 개수를 표시, 그렇지 않으면 빈 문자열
         }
 
-        // 슬롯을 초기화하는 함수 (아이템이 없는 상태로 변경)
+        // 버튼을 초기화하는 메서드
         public void Clean()
         {
-            icon.sprite = null;  // 아이콘 제거
-            icon.gameObject.SetActive(false);  // 아이콘 숨기기
-            text.text = "";  // 텍스트 초기화
-            text.gameObject.SetActive(false);  // 텍스트 숨기기
+            icon.sprite = null;                                             // 아이콘 스프라이트 초기화
+            icon.gameObject.SetActive(false);                               // 아이콘 비활성화
+            text.text = "";                                                 // 텍스트 초기화
+            text.gameObject.SetActive(false);                               // 텍스트 비활성화
         }
 
+        // 포인터 클릭 이벤트 처리 메서드
         public void OnPointerClick(PointerEventData eventData)
         {
-            Debug.Log("클릭했음!");
-            // 클릭 시 해당 슬롯의 아이템을 드래그 앤 드롭 컨트롤러에 전달
-            ItemContainer inventory = GameManager.Instance.inventoryContainer;
-            // 1. 마우스 클릭시 마우스 인벤토리 슬롯이 비어있는지 확인하고 결과에따라 메서드 내용을 실행한다
-            GameManager.Instance.dragAndDropController.OnClick(inventory.slots[myIndex]);
-            // 2. 마우스 클릭을 할 때마다 인벤토리 패널의 내용을 최신화한다
+            // 드래그 앤 드롭 컨트롤러의 클릭 처리 메서드 호출
+            gameManager.dragAndDropController.OnClick(gameManager.inventoryContainer.slots[myIndex]);
+            // 인벤토리 패널을 보여줌
             transform.parent.GetComponent<InventoryPanel>().Show();
         }
 
-        #region TODO :: 드래그 앤 드롭 기능 구현
-        //public void OnBeginDrag(PointerEventData eventData)
-        //{
-        //    // 드래그 시작 시 해당 슬롯의 아이템을 드래그 앤 드롭 컨트롤러에 전달
-        //    ItemContainer inventory = GameManager.Instance.inventoryContainer;
-        //    GameManager.Instance.dragAndDropController.OnClick(inventory.slots[myIndex]);
-        //    transform.parent.GetComponent<InventoryPanel>().Show();
-        //    Debug.Log("드래그 시작!");
-        //}
+        #region 드래그 앤 드롭 기능 구현
+        // 드래그 시작 이벤트 처리 메서드
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            // 현재 슬롯이 비어있으면 리턴
+            if (gameManager.inventoryContainer.slots[myIndex].item == null) return;
+            // 드래그 앤 드롭 컨트롤러의 드래그 시작 메서드 호출
+            gameManager.dragAndDropController.OnDragStart(gameManager.inventoryContainer.slots[myIndex]);
+            // 인벤토리 패널을 보여줌
+            transform.parent.GetComponent<InventoryPanel>().Show();
+        }
 
-        //public void OnDrag(PointerEventData eventData)
-        //{
-        //    Debug.Log("드래그 중!");
-        //    GameManager.Instance.dragAndDropController.ItemIconMoveToMouse();
-        //}
+        // 드래그 중 이벤트 처리 메서드 (현재는 빈 메서드)
+        public void OnDrag(PointerEventData eventData) { }
 
-        //public void OnEndDrag(PointerEventData eventData)
-        //{
-        //    Debug.Log("드래그 끝! 현재 아직 사용하지 않음");
-        //    // 드래그 종료 시 해당 슬롯의 아이템을 드래그 앤 드롭 컨트롤러에 전달
-        //    //ItemContainer inventory = GameManager.Instance.inventoryContainer;
-        //    //GameManager.Instance.dragAndDropController.OnClick(inventory.slots[myIndex]);
-        //    //transform.parent.GetComponent<InventoryPanel>().Show();
-        //}
+        // 드래그 종료 이벤트 처리 메서드
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            // 드래그 중이 아니면 리턴
+            if (!gameManager.dragAndDropController.isDraging) return;
+            // 드래그 앤 드롭 컨트롤러의 드래그 종료 메서드 호출
+            gameManager.dragAndDropController.OnEndDrag();
+        }
 
+        // 드롭 이벤트 처리 메서드
         public void OnDrop(PointerEventData eventData)
         {
-            Debug.Log("드랍");
-            // 드래그 종료 시 해당 슬롯의 아이템을 드래그 앤 드롭 컨트롤러에 전달
-            ItemContainer inventory = GameManager.Instance.inventoryContainer;
-            GameManager.Instance.dragAndDropController.OnClick(inventory.slots[myIndex]);
+            // 드래그 중이 아니면 리턴
+            if (!gameManager.dragAndDropController.isDraging) return;
+            // 드래그 앤 드롭 컨트롤러의 인벤토리에 드롭 메서드 호출
+            gameManager.dragAndDropController.DropInInventoryUI(gameManager.inventoryContainer.slots[myIndex]);
+            // 인벤토리 패널을 보여줌
             transform.parent.GetComponent<InventoryPanel>().Show();
         }
         #endregion
--- Assets/Scripts/Inventory/ItemDragAndDropController.cs
+++ Assets/Scripts/Inventory/ItemDragAndDropController.cs
@@ -1,87 +1,98 @@
-using System.Collections;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
+using UnityEngine; 
+using UnityEngine.EventSystems; 
+using UnityEngine.UI; 
 
-namespace MyStardewValleylikeGame
+namespace MyStardewValleylikeGame 
 {
     public class ItemDragAndDropController : MonoBehaviour
     {
-        [SerializeField] ItemSlot itemSlot;
-        [SerializeField] GameObject itemIcon;
-        RectTransform iconTransform;
-        Image itemIconImage;
+        #region Variables
+        [SerializeField] private GameObject itemIcon;   // 아이템 아이콘을 나타내는 GameObject
+        private ItemSlot itemSlot = new ItemSlot();     // 드래그 및 드롭을 위한 현재 아이템 슬롯
+        private RectTransform iconTransform;            // 아이콘의 RectTransform (UI 위치 및 크기)
+        private Image itemIconImage;                    // 아이콘의 이미지 컴포넌트
+        public bool isDraging = false;                  // 드래그 중인지 여부를 나타내는 플래그
+        #endregion
 
         private void Start()
         {
-            itemSlot = new ItemSlot();
+            //참조 변수
             iconTransform = itemIcon.GetComponent<RectTransform>();
             itemIconImage = itemIcon.GetComponent<Image>();
         }
 
         private void Update()
         {
-
+            // 아이콘이 활성화되어 있으면
             if (itemIcon.activeInHierarchy)
             {
+                // 아이콘의 위치를 마우스 위치로 업데이트
                 iconTransform.position = Input.mousePosition;
 
-                //UI창을 벗어나있는 상태로 클릭하면 아이템을 들고있는 아이템 슬롯을 비우고 아이템을 카메라 위치값에 드롭(Spawn)시킨다.
-                if (Input.GetMouseButtonDown(0))
+                // 마우스 왼쪽 버튼 클릭 시, UI를 벗어난 경우 아이템을 드롭
+                if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
                 {
-                    if (!EventSystem.current.IsPointerOverGameObject())
-                    {
-                        //ScreenToWorldPoint함수를 이용하여 마우스의 위치값을 3D 월드의 위치값으로 변환
-                        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                        //z값을 0으로 설정한다
-                        worldPosition.z = 0f;
-                        //아이템을 드롭시킨다.
-                        ItemSpawnManager.Instance.SpawnItem(worldPosition, itemSlot.item, itemSlot.count);
-                        //아이템 슬롯을 비운다.
-                        itemSlot.Clear();
-                        //아이콘을 비활성화한다.
-                        UpdateIcon();
-                    }
+                    DropItem(); // 아이템 드롭 메서드 호출
                 }
             }
         }
 
-        public void OnClick(ItemSlot itemSlot)
+        // 슬롯 클릭 시 호출되는 메서드
+        public void OnClick(ItemSlot clickedSlot)
         {
-            if (this.itemSlot.item == null)
+            // 현재 슬롯에 아이템이 없으면
+            if (itemSlot.item == null)
             {
-                ////마우스 클릭시 마우스 인벤토리 슬롯이 비어있으면 인벤토리 슬롯안에 내용을 마우스 인벤토리 슬롯으로 복사해준다.
-                this.itemSlot.Copy(itemSlot);
-                itemSlot.Clear();
+                itemSlot.Copy(clickedSlot);        // 클릭한 슬롯의 아이템 정보를 복사
+                clickedSlot.Clear();               // 클릭한 슬롯을 비움
             }
-            else
+            else // 현재 슬롯에 아이템이 있을 경우
             {
-                //마우스 클릭시 마우스 인벤토리 슬롯이 비어있지않으면 마우스 안에있는 내용과 인벤토리 슬롯안에 내용을 바꿔준다.
-                Item item = itemSlot.item;
-                int count = itemSlot.count;
-
-                //클릭한 아이템 슬롯에 마우스 아이템 슬롯의 내용을 복사
-                itemSlot.Copy(this.itemSlot);
-                //마우스 아이템 슬롯에 클릭한 아이템 슬롯의 내용을 복사
-                this.itemSlot.Set(item, count);
+                Item tempItem = clickedSlot.item;  // 클릭한 슬롯의 아이템을 임시 변수에 저장
+                int tempCount = clickedSlot.count; // 클릭한 슬롯의 아이템 개수를 임시 변수에 저장
+                clickedSlot.Copy(itemSlot);        // 현재 슬롯의 아이템 정보를 클릭한 슬롯으로 복사
+                itemSlot.Set(tempItem, tempCount); // 임시 변수의 아이템 정보를 현재 슬롯에 설정
             }
-
-            UpdateIcon();
+            UpdateIcon();                          // 아이콘 업데이트 메서드 호출
         }
 
+        // 아이콘 업데이트 메서드
         private void UpdateIcon()
         {
-            //현재 마우스 인벤토리 슬롯에 아이템이 없으면 아이콘을 비활성화 / 있으면 활성화
-            if (this.itemSlot.item == null)
-            {
-                itemIconImage.sprite = null;
-                itemIcon.SetActive(false);
-            }
-            else
-            {
-                itemIconImage.sprite = this.itemSlot.item.icon;
-                itemIcon.SetActive(true);
-            }
+            itemIcon.SetActive(itemSlot.item != null);                            // 현재 슬롯에 아이템이 없으면 아이콘 비활성화, 있으면 활성화
+            if (itemSlot.item != null) itemIconImage.sprite = itemSlot.item.icon; // 아이콘 이미지 설정
         }
+        #region 드래그 앤 드롭 기능 구현
+        // 드래그 시작 시 호출되는 메서드
+        public void OnDragStart(ItemSlot draggedSlot)
+        {
+            isDraging = true;     // 드래그 중 플래그 설정
+            OnClick(draggedSlot); // 드래그하는 슬롯 클릭 처리
+        }
+
+        // 인벤토리 UI에 드롭 시 호출되는 메서드
+        public void DropInInventoryUI(ItemSlot targetSlot)
+        {
+            isDraging = false;   // 드래그 중 플래그 해제
+            OnClick(targetSlot); // 타겟 슬롯 클릭 처리
+        }
+
+        // 드래그 종료 시 호출되는 메서드
+        public void OnEndDrag()
+        {
+            isDraging = false; // 드래그 중 플래그 해제
+            DropItem();        // 아이템 드롭 메서드 호출
+        }
+
+        // 아이템 드롭 메서드
+        private void DropItem()
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);        // 마우스 위치를 3D 월드 좌표로 변환
+            worldPosition.z = 0f;                                                               // z 값 0으로 설정 (2D 게임을 위해)
+            ItemSpawnManager.Instance.SpawnItem(worldPosition, itemSlot.item, itemSlot.count);  // 아이템을 월드에 드롭
+            itemSlot.Clear();                                                                   // 현재 슬롯 비우기
+            UpdateIcon();                                                                       // 아이콘 업데이트 메서드 호출
+        }
+        #endregion
     }
 }

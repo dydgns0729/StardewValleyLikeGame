e4e834e989ff6f9260d8f98d142ab71624584d4b
toneed <dydgns0729@gmail.com>
2025-03-18 23:42:47 +0900

    1. 다이얼로그 시스템 구현 완료

--- Assets/Scripts/DialogueContainer.cs
+++ Assets/Scripts/DialogueContainer.cs
@@ -6,7 +6,9 @@ namespace MyStardewValleylikeGame
     [CreateAssetMenu(menuName = "Data/Dialogue/Dialogue")]
     public class DialogueContainer : ScriptableObject
     {
+        // NPC가 보여줄 대화내용
         public List<string> line;
+        // NPC
         public Actor actor;
     }
 }
--- Assets/Scripts/Interactable/TalkInteract.cs
+++ Assets/Scripts/Interactable/TalkInteract.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 namespace MyStardewValleylikeGame
 {
+    /// <summary>
+    /// NPC와 대화를 상호작용할 수 있도록 하는 클래스
+    /// </summary>
     public class TalkInteract : Interactable
     {
+        // NPC와의 대화 내용을 담고 있는 대화 컨테이너 (ScriptableObject)
         [SerializeField] DialogueContainer dialogue;
 
+        /// <summary>
+        /// 플레이어가 NPC와 상호작용할 때 실행되는 메서드
+        /// </summary>
         public override void Interact()
         {
+            // GameManager의 대화 시스템을 통해 대화를 시작
             GameManager.Instance.dialogueSystem.Initialize(dialogue);
         }
     }
